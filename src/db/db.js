920441edd39b2d68af00904f0db4629921cee5f8
Jhosue900 <jhosuellanovargas@gmail.com>
2023-02-20 18:37:56 -0500

    Tercer commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
+    connectionString: 'postgres:a5aVBHyaAYTRZ913j2c8@containers-us-west-56.railway.app:7534/railway',
+    ssl: {
+      rejectUnauthorized: false
+    },
     user: "postgres",
     password: "a5aVBHyaAYTRZ913j2c8",
     host: "containers-us-west-56.railway.app",
